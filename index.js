16ecade3f87f5fdd4c83a82c9f7896b3eab89e01
nickph <nic.prudhomme3@gmail.com>
2017-11-30 22:06:31 -0500

    few layout adjustment before branching out to test firebase

--- index.js
+++ index.js
@@ -10,15 +10,30 @@ let size = "300/300";
 let propositions = [
     {
         title: 'Make a skating rink at lafontaine park',
-        image: "?image=1076",
-        author: 'Mark',
+        image: "image=1076",
+        author: 'Mark Rivas',
         description: 'Instead of draining all the water, we could make the park a winter wonderland'
     },
     {
         title: 'The construction of the new Radio Canada tower generate a lot of traffic',
-        image: "?image=1076",
-        author: 'Mirela',
+        image: "image=1078",
+        author: 'Mirela Drew',
         description: 'Some of the equipment could be moved out from the street'
+    },
+    {
+        title: 'University accessible to anyone',
+        image: 'image=1073',
+        author: 'Joy Davis'
+    },
+    {
+        title: 'Idea for place emily gamelin',
+        image: 'image=1080',
+        author: 'Melissa Webb'
+    },
+    {
+        title: 'Condominium development in hochelaga',
+        image: 'image=1052',
+        author: 'Kennett Leary'
     }
 ];
 
@@ -58,3 +73,4 @@ app.get("*", function(req, res){
 app.listen(3000, function(){
     console.log("listening on local host port 3000");
 });
+
